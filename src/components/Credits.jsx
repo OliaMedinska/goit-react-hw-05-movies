39f97aa42d74d2bc82fe49160686b42849ce0784
OliaMedinska <medinska2@gmail.com>
2023-12-11 22:51:45 +0100

    pages

--- src/components/Credits.jsx
+++ src/components/Credits.jsx
@@ -24,7 +24,7 @@ export default function Credits() {
     }
 
     getCredits();
-  }, []);
+  }, [movieId]);
 
   return (
     <>
--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -24,7 +24,7 @@ export default function Reviews() {
     }
 
     getReviews();
-  }, []);
+  }, [movieId]);
 
   return (
     <>

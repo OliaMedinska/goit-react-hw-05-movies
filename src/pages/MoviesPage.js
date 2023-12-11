0d3ee618a4efbbdc641a6b9e21ed238da6fa17a6
OliaMedinska <medinska2@gmail.com>
2023-12-11 22:37:45 +0100

    pages

--- src/pages/MoviesPage.js
+++ src/pages/MoviesPage.js
@@ -1,68 +1,69 @@
-import {searchMovies} from 'api';
-import { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+// import {searchMovies} from 'api';
+// import { useState, useEffect } from 'react';
+// import { Link } from 'react-router-dom';
 
 export default function MoviesPage () {
-    const [movieItems, setMovieItems] = useState([]);
-    const [movieName, setMovieName] = useState('');
-    const [page, setPage] = useState(1);
-    const [isLoading, setIsLoading] = useState(false);
+//     const [movieItems, setMovieItems] = useState([]);
+//     const [movieName, setMovieName] = useState('');
+//     const [page, setPage] = useState(1);
+//     const [isLoading, setIsLoading] = useState(false);
 
-useEffect(() => {
-    async function getMovies() {
-        if (movieName === '') {
-        return;
-        }
+// useEffect(() => {
+//     async function getMovies() {
+//         if (movieName === '') {
+//         return;
+//         }
 
-        try {
-        const searchedMovies = await searchMovies(movieName, page);
-        console.log(searchMovies);
-        setIsLoading(true);
-        setMovieItems(prevItems => [...prevItems, ...searchedMovies]);
-        } catch (error) {
-        console.log('Something wrong...');
-        } finally {
-        setIsLoading(false);
-        }
-    }
+//         try {
+//         const searchedMovies = await searchMovies(movieName, page);
+//         console.log(searchMovies);
+//         setIsLoading(true);
+//         setMovieItems(prevItems => [...prevItems, ...searchedMovies]);
+//         } catch (error) {
+//         console.log('Something wrong...');
+//         } finally {
+//         setIsLoading(false);
+//         }
+//     }
 
-    getMovies();
-    }, [movieName, page]);
+//     getMovies();
+//     }, [movieName, page]);
 
 
-const onSubmitItems = e => {
-    e.preventDefault();
+// const onSubmitItems = e => {
+//     e.preventDefault();
 
-    setMovieItems([]);
-    setMovieName(e.target.search.value.trim());
-    setPage(1);
-};
+//     setMovieItems([]);
+//     setMovieName(e.target.search.value.trim());
+//     setPage(1);
+// };
 
     return (
-        <>
-        <form onSubmit={onSubmitItems}>
-            <input
-             type="text"
-             autoComplete="off"
-             autoFocus
-             placeholder="Search movies"
-             name="search"
-            />
+        <div>movies</div>
+//         <>
+//         <form onSubmit={onSubmitItems}>
+//             <input
+//              type="text"
+//              autoComplete="off"
+//              autoFocus
+//              placeholder="Search movies"
+//              name="search"
+//             />
             
 
-            <button type="submit">
-                search
-            </button>
-        </form>
+//             <button type="submit">
+//                 search
+//             </button>
+//         </form>
 
-        <ul>
-        {movieItems.map(({title,name,id}) => (
-          <li key={id}>
-            <Link to={`/movies/${id}`}>{title || name}</Link>
-          </li>
-        ))}
-      </ul>
-        </>
+//         <ul>
+//         {movieItems.map(({title,name,id}) => (
+//           <li key={id}>
+//             <Link to={`/movies/${id}`}>{title || name}</Link>
+//           </li>
+//         ))}
+//       </ul>
+//         </>
     )
 };
 

44b32341d0a3e02ed9a2ca2463b3b109a0785865
OliaMedinska <medinska2@gmail.com>
2023-12-12 16:18:59 +0100

    last changes

--- src/pages/MoviesPage/MoviesPage.js
+++ src/pages/MoviesPage/MoviesPage.js
@@ -58,6 +58,7 @@ const onSubmitItems = e => {
             <ItemLink to={`/movies/${id}`}>{title || name}</ItemLink>
           </li>
         ))}
+        {isLoading}
       </ItemsList>
         </>
     )
